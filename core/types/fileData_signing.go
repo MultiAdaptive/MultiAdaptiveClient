a074de7dd9eacec9f639cbd25b097586e3976581
WuEcho <emailforecho@163.com>
2024-08-14 17:38:06 +0800

    delete log

--- core/types/fileData_signing.go
+++ core/types/fileData_signing.go
@@ -144,8 +144,6 @@ func (s EIP155DASigner) Sender(fd *DA) ([]common.Address, []error) {
 	for _, signData := range fd.SignData {
 		log.Info("Sender----signData","signData",common.Bytes2Hex(signData))
 		R, S, V := sliteSignature(signData)
-		signHash := s.Hash(fd)
-		log.Info("Sender----signData--2","signHash",signHash.Hex())
 		addr, err := recoverPlain(s.Hash(fd), R, S, V, true)
 		if err != nil {
 			errors = append(errors, err)
